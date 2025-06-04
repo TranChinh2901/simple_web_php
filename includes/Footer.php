5c7f29607d419ac39d92e6fcd8d48cc8ec21adfa
TranChinh2901 <tranchinht32901@gmail.com>
2025-06-04 13:42:25 +0700

    fix loi login

--- includes/Footer.php
+++ includes/Footer.php
@@ -9,6 +9,13 @@
 
 <body>
     <h1>FOoter</h1>
+    <footer>
+        <p>&copy; 2023 Cael Stratos. All rights reserved.</p>
+        <p>Follow us on:
+            <a href="https://www.facebook.com" target="_blank">Facebook</a>,
+            <a href="https://www.twitter.com" target="_blank">Twitter</a>,
+            <a href="https://www.instagram.com" target="_blank">Instagram</a>
+        </p>
 </body>
 
 </html>
